c209828ff81348283c62fc7640da46f1c433ea41
leminhdung2701 <leminhdung_t64@hus.edu.vn>
2022-01-09 23:33:11 +0700

    upadte

--- project/test.py
+++ project/test.py
@@ -5,5 +5,10 @@ app = Flask(__name__,template_folder='template',static_folder='static',)
 def demo():
     return render_template('demo.html')
 
+@app.route('/submit',methods=['POST','GET'])
+def predict():
+    return render_template('demo.html')
+
+
 if __name__=="__main__":
     app.run(debug=True)
