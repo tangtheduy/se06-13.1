a48ad666d87a35ea037a964fa96d2c3ab4d6d27a
leminhdung2701 <leminhdung_t64@hus.edu.vn>
2022-01-09 22:16:01 +0700

    update demo file

---
+++ project/project/test.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template
+app = Flask(__name__,template_folder='template',static_folder='static',)
+
+@app.route("/")
+def demo():
+    return render_template('demo.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
